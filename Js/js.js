9ff14b5a8b9512237120f1f211dcb35d2417b8a4
dejanrojin <dejan.rojin@yahoo.com>
2020-07-03 18:04:25 +0200

    Update js.js

--- Js/js.js
+++ Js/js.js
@@ -78,22 +78,22 @@ secondss="0"+secondss;
 
 
 
-var countDownDate = new Date("Jun 25, 2021 15:37:25").getTime();
+var countDownDate2 = new Date("Jun 25, 2021 15:37:25").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x2 = setInterval(function() {
 
   // Get today's date and time
-  var now = new Date().getTime();
+  var now2 = new Date().getTime();
     
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  var distance2 = countDownDate2 - now2;
     
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var days = Math.floor(distance2 / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance2 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance2 % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
   if(seconds < 10){
@@ -106,7 +106,7 @@ seconds="0"+seconds;
 
   // If the count down is over, write some text 
   if (distance < 0) {
-    clearInterval(x);
+    clearInterval(x2);
     document.getElementById("demo2").innerHTML = "EXPIRED";
   }
 }, 1000);
