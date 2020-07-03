329d929b8a8ace335f977a60081df0dc722d7845
dejanrojin <dejan.rojin@yahoo.com>
2020-07-03 17:56:01 +0200

    Update js.js

--- Js/js.js
+++ Js/js.js
@@ -1,7 +1,7 @@
 var countDownDate3 = new Date("Avg 25, 2021 15:37:25").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x3 = setInterval(function() {
 
   // Get today's date and time
   var now3 = new Date().getTime();
@@ -15,10 +15,10 @@ var x = setInterval(function() {
   var minutesss = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var secondsss = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Output the result in an element with id="demo"
-  if(secondss < 10){
+  // Output the result in an element with id="demo3"
+  if(secondsss < 10){
 	
-secondss="0"+secondss;
+secondsss="0"+secondsss;
 }    
 	
   document.getElementById("demo3").innerHTML = daysss + "d " + hoursss + "h "
@@ -26,10 +26,13 @@ secondss="0"+secondss;
 
   // If the count down is over, write some text 
   if (distance3 < 0) {
-    clearInterval(x);
+    clearInterval(x3);
     document.getElementById("demo3").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+
+
 
 // Set the date we're counting down to
 var countDownDate = new Date("Avg 25, 2021 15:37:25").getTime();
